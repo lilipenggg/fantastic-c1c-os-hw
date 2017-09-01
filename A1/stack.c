d9fb8689286d47b763ff1cf177baceb28beeb5fc
Lili Peng <lilipeng@mail.weber.edu>
2017-08-31 22:47:00 -0600

    fixed bugs in stack.c file

--- A1/stack.c
+++ A1/stack.c
@@ -1,82 +1,83 @@
-#include <studio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "stack.h"
 
-struct stackT *NewStack(void)
+stackT *NewStack(void)
 {
-	stackT *newStackPtr = mallof(sizeof(stackT))
-	newStackPtr->head = NULL;
-	return newStackPtr;
+    stackT *newStackPtr = malloc(sizeof(stackT));
+    newStackPtr->head = NULL;
+    return newStackPtr;
 }
 
 void Push(stackT *stack, valueT value)
 {
     nodeT *newNode = malloc(sizeof(nodeT));
-	newNode->value = value;
-	if (stack->head == NULL)
-	{
-		stack->head = newNode;
-		newNode->next = NULL;
-	}
-	else
-	{
-		newNode->next = head;
-		head = newNode;
-	}
+    newNode->value = value;
+    if (stack->head == NULL)
+    {
+        stack->head = newNode;
+        newNode->next = NULL;
+    }
+    else
+    {
+        newNode->next = stack->head;
+        stack->head = newNode;
+    }
 }
 
 valueT Pop(stackT *stack)
 {
-	if (stack->head == NULL)
-	{
-		printf("Error: Nothing can be popped because this is an empty stack.")
-			return '0';
-	}
-	else
-	{
-		nodeT *tempNode = stack->head;
-		stack->head = head->next;
-		delete tempNode;
-	}
+    if (stack->head == NULL)
+    {
+        printf("Error: Nothing can be popped because this is an empty stack.\n");
+        return 0;
+    }
+    else
+    {
+        valueT tempVal = stack->head->value;
+        nodeT *tempNode = stack->head;
+        stack->head = stack->head->next;
+        free(tempNode);
+        return tempVal;
+    }
 }
 
 void EmptyStack(stackT *stack)
 {
-	if (stack->head == NULL)
-	{
-		printf("Error: The stack is empty.")
-		return 0;
-	}
-	else
-	{
-		nodeT *currNode = stack->head;
-		while (currNode->next != NULL)
-		{
-			nodeT *tempNode = currNode;
-			currNode = currNode->next;
-			delete tempNode;
-		}
-		delete currNode; // delete the last node in the stack
-		stack->head = NULL;
-	}
+    if (stack->head == NULL)
+    {
+        printf("Error: The stack is empty.\n");
+    }
+    else
+    {
+        nodeT *currNode = stack->head;
+        while (currNode->next != NULL)
+        {
+            nodeT *tempNode = currNode;
+            currNode = currNode->next;
+            free(tempNode);
+        }
+        free(currNode); // delete the last node in the stack
+        stack->head = NULL;
+    }
 }
 
 void FreeStack(stackT *stack)
 {
-	if (stack->head != NULL)
-	{
-		printf("Error: The stack cannot be free because it is not an empty stack.");
-		return 0;
-	}
-	else
-	{
-		stack->head = NULL;
-		delete stack;
-	}
+    if (stack->head != NULL)
+    {
+        printf("Error: The stack cannot be free because it is not an empty stack.\n");
+    }
+    else
+    {
+        stack->head = NULL;
+        free(stack);
+    }
 }
 
 bool IsEmpty(stackT *stack)
 {
-    enum bool flag = true;
+    bool flag = true;
     if(stack->head != NULL)
     {
         flag = false;
